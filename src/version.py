e5036549aa66029b720bdc2d76c212efed9cb030
Pedro Sgarbi <peusgarbi@gmail.com>
2026-06-29 06:14:29 -0300

    feat: release version 1.3.0

--- src/version.py
+++ src/version.py
@@ -1,4 +1,4 @@
-VERSION = "1.2.9"
+VERSION = "1.3.0"
 REPOSITORY = "peusgarbi/pastasautomaticas"
 LATEST_RELEASE_URL = f"https://api.github.com/repos/{REPOSITORY}/releases/latest"
 REPOSITORY_URL = f"https://github.com/{REPOSITORY}"
